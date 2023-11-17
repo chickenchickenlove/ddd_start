65824dad48456e907f51b194dd9f237b054dce44
ChickenchickenLove <ojt90902@naver.com>
2023-11-18 00:26:25 +0900

    git init

---
+++ src/main/java/com/me/dddstudy/DddstudyApplication.java
@@ -0,0 +1,13 @@
+package com.me.dddstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddstudyApplication.class, args);
+	}
+
+}
